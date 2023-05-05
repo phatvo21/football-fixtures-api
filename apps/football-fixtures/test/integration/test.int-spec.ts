529e4eeb1414ce8d79f1ff9c0a8396b66e9b6272
Phat Vo <phat@briohr.com>
2023-05-05 09:45:07 +0700

    Add testing

---
+++ apps/football-fixtures/test/integration/test.int-spec.ts
@@ -0,0 +1,49 @@
+/* eslint-disable max-lines */
+import { BaseModule } from '@app/common/modules';
+import {
+  generateMockServer,
+  generateRequest, getEntity,
+  RequestType, ServerType,
+} from '@app/common/utils/database-test.util';
+import { TestEntity } from '@app/football-fixtures/fixture/data/entities/test.entity';
+import { FixtureModule } from '@app/football-fixtures/fixture/fixture.module';
+
+describe('notifications (e2e)', () => {
+  let server: ServerType;
+  let request: RequestType;
+  let entity: any;
+
+  beforeAll(async () => {
+    server = await generateMockServer([
+      BaseModule,
+      FixtureModule,
+    ]);
+    request = generateRequest(server);
+    entity = server.module.get(getEntity(TestEntity));
+  });
+
+  afterAll(async () => {
+    await server.app.close();
+  });
+
+  describe('Test', () => {
+    beforeAll(async () => {
+      const data = new TestEntity();
+      data.createdAt = new Date();
+      data.updatedAt = new Date();
+      await entity.save(data)
+    });
+
+    afterAll(async () => {
+      await entity.clear();
+    });
+
+    it('should return list of all notifications for a given user and specific channel', async () => {
+      // eslint-disable-next-line no-secrets/no-secrets
+      const res = await request.agent.get(`/test`);
+      const result = await entity.find();
+      console.log(result);
+      expect(res.status).toEqual(200);
+    });
+  });
+});
--- libs/common/src/utils/database-test.util.ts
+++ libs/common/src/utils/database-test.util.ts
@@ -1,56 +1,49 @@
-// import { getAdapter } from '@app/common/utils/fastify.util';
-// import { NestFastifyApplication } from '@nestjs/platform-fastify';
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { getModelToken } from '@nestjs/mongoose';
-// import { exec } from 'child_process';
-// import fastifyCookie from 'fastify-cookie';
-// import fastifyCsrf from 'fastify-csrf';
-// import supertest from 'supertest';
-// // eslint-disable-next-line unicorn/import-style
-// import util from 'util';
-//
-// export const execAsync = util.promisify(exec);
-//
-// export const restoreDb = async (database = 'simple-api.archive', collection = '*', dbPath = 'database') => {
-//   console.info('Begin restore', database, collection);
-//   const path = `${process.cwd()}/${dbPath}`;
-//
-//   await execAsync(
-//     `mongorestore --host localhost:27017 -u mongo -p mongo --authenticationDatabase admin --nsInclude="simple-api.${collection}" --archive=${path}/${database} --drop --noIndexRestore --quiet --numParallelCollections=4 --numInsertionWorkersPerCollection=2`,
-//   );
-//   console.info('Restored:', database, collection);
-// };
-// export interface ServerType {
-//   app: NestFastifyApplication;
-// }
-// export interface RequestType {
-//   agent: supertest.SuperTest<supertest.Test>;
-// }
-//
-// export const generateRequest = (server: { app: NestFastifyApplication }): RequestType => {
-//   const agent = supertest.agent(server.app.getHttpServer());
-//   return {
-//     agent,
-//   };
-// };
-//
-// export const generateMockServer = async (modules = []) => {
-//   const moduleFixture: TestingModule = await Test.createTestingModule({
-//     imports: modules,
-//   }).compile();
-//
-//   const adapter = getAdapter();
-//   const app = moduleFixture.createNestApplication<NestFastifyApplication>(adapter);
-//
-//   await app.register(fastifyCookie);
-//   await app.register(fastifyCsrf);
-//   await app.init();
-//   await app.getHttpAdapter().getInstance().ready();
-//   return {
-//     app,
-//   };
-// };
-//
-// export const getModel = (server: { app: NestFastifyApplication }, entityName: string) => getModelToken(entityName);
-//
-// export const wait = async (time = 500) => new Promise((resolve) => setTimeout(() => resolve(''), time));
+import { getAdapter } from '@app/common/utils/fastify.util';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+import { exec } from 'child_process';
+import fastifyCookie from 'fastify-cookie';
+import fastifyCsrf from 'fastify-csrf';
+import supertest from 'supertest';
+// eslint-disable-next-line unicorn/import-style
+import util from 'util';
+
+export const execAsync = util.promisify(exec);
+export interface ServerType {
+  app: NestFastifyApplication;
+  module: TestingModule
+}
+export interface RequestType {
+  agent: supertest.SuperTest<supertest.Test>;
+}
+
+export const generateRequest = (server: { app: NestFastifyApplication }): RequestType => {
+  const agent: supertest.SuperAgentTest = supertest.agent(server.app.getHttpServer());
+  return {
+    agent,
+  };
+};
+
+export const generateMockServer = async (modules: any[] = []): Promise<ServerType> => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: modules,
+  }).compile();
+
+  const adapter: FastifyAdapter = getAdapter();
+  const app: NestFastifyApplication = moduleFixture.createNestApplication<NestFastifyApplication>(adapter);
+
+  await app.register(fastifyCookie);
+  await app.register(fastifyCsrf);
+  await app.init();
+  await app.getHttpAdapter().getInstance().ready();
+  return {
+    app,
+    module: moduleFixture
+  };
+};
+
+export const getEntity = <T extends EntityClassOrSchema>(entity: T) => getRepositoryToken(entity);
+
+export const wait = async (time = 500): Promise<unknown> => new Promise((resolve) => setTimeout(() => resolve(''), time));
